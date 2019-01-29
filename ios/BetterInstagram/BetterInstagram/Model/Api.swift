0c61bb66ff4c83ff0c23afc2d7b24bff0deb28d3
Dhawal Majithia <deviolxd@icloud.com>
2019-01-28 20:31:43 -0800

    app, api, database comm working\!

--- ios/BetterInstagram/BetterInstagram/Model/Api.swift
+++ ios/BetterInstagram/BetterInstagram/Model/Api.swift
@@ -7,33 +7,36 @@
 //
 
 import Foundation
-import OHMySQL
+import SwiftyJSON
+import Alamofire
+import FacebookCore
 
 struct Api{
     
     static func checkIfUserExists(fbID: String) -> Bool{
-        let user = OHMySQLUser(userName: "dbmasteruser", password: "12345678", serverName: "ls-4c577f8ce2558da6e77b799294f2a69c0d455270.cyxr3j60i1wl.us-west-2.rds.amazonaws.com", dbName: "dbmaster", port: 3306, socket: "")
-        let coordinator = OHMySQLStoreCoordinator(user: user!)
-        coordinator.encoding = .UTF8MB4
-        coordinator.connect()
-        let queryString = "SELECT user_id, username, facebook_id FROM user WHERE user_id = 1"
-        //let queryString = OHMySQLQueryRequestFactory.selectFirst("user", condition: "").queryString
-        let query = OHMySQLQueryRequest.init(queryString: queryString)
-        let response = try? OHMySQLContainer.shared.mainQueryContext?.executeQueryRequestAndFetchResult(query)
-        NSLog(response.debugDescription)
+        AF.request("http://127.0.0.1:5000/?fbID="+(AccessToken.current?.userId)!+"&function=check", method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        //NSLog(response.debugDescription)
         return false
     }
     
     static func addUserToDatabase(fbID: String, username: String){
-        let user = OHMySQLUser(userName: "dbmasteruser", password: "12345678", serverName: "ls-4c577f8ce2558da6e77b799294f2a69c0d455270.cyxr3j60i1wl.us-west-2.rds.amazonaws.com", dbName: "dbmaster", port: 3306, socket: "")
-        let coordinator = OHMySQLStoreCoordinator(user: user!)
-        coordinator.encoding = .UTF8MB4
-        coordinator.connect()
-        //let queryString = "INSERT INTO user SET username = " + username + ", facebook_id = " + fbID
-        let queryString = OHMySQLQueryRequestFactory.insert("user", set: ["username":username, "facebook_id":fbID]).queryString
-        let query = OHMySQLQueryRequest.init(queryString: queryString)
-        let response = try? OHMySQLContainer.shared.mainQueryContext?.executeQueryRequestAndFetchResult(query)
-        NSLog(response.debugDescription)
+        AF.request("http://127.0.0.1:5000/?fbID="+fbID+"&function=insert&username="+username, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     
--- python/project/hello_flask/app.py
+++ python/project/hello_flask/app.py
@@ -1,23 +1,47 @@
 from flask import Flask
 from flask import request
 import pymysql
+import json
 		
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+ fun = request.args.get('function')
  fbID = request.args.get('fbID')
  username = request.args.get('username')
- db = pymysql.connect("ls-4c577f8ce2558da6e77b799294f2a69c0d455270.cyxr3j60i1wl.us-west-2.rds.amazonaws.com", "dbmasteruser", "12345678", "dbmaster")
- cursor = db.cursor()
- sql = "INSERT INTO user (username, facebook_id) VALUES( '" + username + "', '" + fbID + "');"
- try:
-  cursor.execute(sql)
-  db.commit()
- except:
-  db.rollback()
- db.close()
- return 'Hello, Flask!'
+ if fun == "check":
+ 	db = pymysql.connect("ls-4c577f8ce2558da6e77b799294f2a69c0d455270.cyxr3j60i1wl.us-west-2.rds.amazonaws.com", "dbmasteruser", "12345678", "dbmaster")
+ 	cursor = db.cursor()
+ 	sql = "SELECT user_id, username, facebook_id  from user WHERE facebook_id = '" + fbID + "'"
+ 	try:
+  	  cursor.execute(sql)
+  	  result = cursor.fetchone()
+  	  db.commit()
+ 	except:
+  	  db.rollback()
+ 	db.close()
+ 	if result == None:
+ 		dic = {'status': False}
+ 	else:
+ 		dic = {'status': True,'user_id':result[0], 'username':result[1], 'facebook_id':result[2]}
+ 	js = json.dumps(dic)
+ 	return js
+ elif fun =="insert":
+ 	db = pymysql.connect("ls-4c577f8ce2558da6e77b799294f2a69c0d455270.cyxr3j60i1wl.us-west-2.rds.amazonaws.com", "dbmasteruser", "12345678", "dbmaster")
+ 	cursor = db.cursor()
+ 	sql = "INSERT INTO user (username, facebook_id) VALUES( '" + username + "', '" + fbID + "');"
+ 	try:
+  	  cursor.execute(sql)
+  	  db.commit()
+ 	except:
+  	  db.rollback()
+ 	db.close()
+
 
 if __name__ == '__main__':
  app.run(debug=True)
+
+
+
+
