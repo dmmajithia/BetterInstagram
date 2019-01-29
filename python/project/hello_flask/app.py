725a83aef365f181f269bdbc23593c9d46b4df14
Dhawal Majithia <deviolxd@icloud.com>
2019-01-28 19:56:31 -0800

    local flask working

--- python/project/hello_flask/app.py
+++ python/project/hello_flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import Request
+from flask import request
+import pymysql
 		
 app = Flask(__name__)
 
@@ -7,6 +8,15 @@ app = Flask(__name__)
 def index():
  fbID = request.args.get('fbID')
  username = request.args.get('username')
+ db = pymysql.connect("ls-4c577f8ce2558da6e77b799294f2a69c0d455270.cyxr3j60i1wl.us-west-2.rds.amazonaws.com", "dbmasteruser", "12345678", "dbmaster")
+ cursor = db.cursor()
+ sql = "INSERT INTO user (username, facebook_id) VALUES( '" + username + "', '" + fbID + "');"
+ try:
+  cursor.execute(sql)
+  db.commit()
+ except:
+  db.rollback()
+ db.close()
  return 'Hello, Flask!'
 
 if __name__ == '__main__':
