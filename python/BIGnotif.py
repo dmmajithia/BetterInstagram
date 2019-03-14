8ea28ff28d26795e691f592e19b9d8316c8f34b5
zromade <47095488+zromade@users.noreply.github.com>
2019-03-13 23:16:43 -0700

    added restapi key

--- python/BIGnotif.py
+++ python/BIGnotif.py
@@ -2,7 +2,7 @@
 
 from onesignal import OneSignal, SegmentNotification, FilterNotification, Filter
 
-client = OneSignal("b93dcd48-6999-4353-8c4d-4a81b063f8c4", "RestAPI Key Here")
+client = OneSignal("b93dcd48-6999-4353-8c4d-4a81b063f8c4", "NDgwMTFmZmUtNmVkYi00YzFhLTg0ZWUtZmMyYjU2ZGU3M2U3")
 
 def PushToAll(message):
     nf_to_all = SegmentNotification(
