158db9afed02a3b96aa1b09520debaea8e15d8b9
zromade <47095488+zromade@users.noreply.github.com>
2019-03-15 17:35:51 -0700

    add try block to function, return false if fail

--- python/BIGnotif.py
+++ python/BIGnotif.py
@@ -4,16 +4,19 @@ from onesignal import OneSignal, SegmentNotification, FilterNotification, Filter
 
 client = OneSignal("b93dcd48-6999-4353-8c4d-4a81b063f8c4", "NDgwMTFmZmUtNmVkYi00YzFhLTg0ZWUtZmMyYjU2ZGU3M2U3")
 
-def PushToAll(message):
-    nf_to_all = SegmentNotification(
-        contents= {"en": message},
-        included_segments=SegmentNotification.ALL
-    )
-    client.send(nf_to_all)
+# def PushToAll(message):
+#     nf_to_all = SegmentNotification(
+#         contents= {"en": message},
+#         included_segments=SegmentNotification.ALL
+#     )
+#     client.send(nf_to_all)
 
 def PushToUser(appID, username, message):
-    nf_to_some = FilterNotification(
-        contents={"en": username + message},
-        filters=[Filter.Tag("appID", "=", appID),]
-    )
-    client.send(nf_to_some)
+    try:
+        nf_to_some = FilterNotification(
+            contents={"en": username + message},
+            filters=[Filter.Tag("appID", "=", appID),]
+        )
+        client.send(nf_to_some)
+    except:
+        return false
